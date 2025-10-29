329ff39ebda7e0ddcef7ce4f982c4b75c5957c59
JoaoMuraiviechi <joao.muraiviechi@gmail.com>
2025-10-29 20:58:52 -0300

    Duplicando projeto MongoDB para PostgreSQL

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { register, login, protectedRoute } from "../controllers/authController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { health } from "../controllers/healthController";
+
+const router = Router();
+
+router.get("/", health);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/protected", authMiddleware, protectedRoute);
+
+export default router;
